97636e97259102b0554495ba96dac6bcca495d66
godsejeong <dbdnpxm@naver.com>
2020-04-17 17:41:48 +0900

    reload youtube view

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/material.dart';
+import 'dart:developer';
+import 'package:flutter/foundation.dart';
 import 'package:youtube_player_flutter/youtube_player_flutter.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-
+  
+  BuildContext ctx;
+  
   @override
   Widget build(BuildContext context) {
+    ctx = context;
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
@@ -29,20 +33,22 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   TextEditingController _idController = TextEditingController();
-  String id = 'eDEFolvLn0A';
+  static String id = 'eDEFolvLn0A';
 
   YoutubePlayerController _controller = YoutubePlayerController(
-    initialVideoId: 'eDEFolvLn0A',
-    flags: YoutubePlayerFlags(autoPlay: true)
+    initialVideoId: id,
+    flags: YoutubePlayerFlags(autoPlay: true),
   );
 
   @override
   Widget build(BuildContext context) {
+  TextEditingController _idController = TextEditingController();
     return Scaffold(
         appBar: AppBar(
           title: Text(widget.title),
         ),
-        body: SingleChildScrollView(
+        body: 
+        SingleChildScrollView(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.start,
             children: <Widget>[
@@ -50,9 +56,35 @@ class _MyHomePageState extends State<MyHomePage> {
               controller: _controller,
               liveUIColor: Colors.amber,
               ),
+
+             Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: TextField(
+                controller: _idController,
+                decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Input Youtobe Video Code',
+                )
+              ),
+            ),
+
+            RaisedButton(
+                onPressed: () {
+                  setState(() {
+                    id = _idController.text;
+                    debugPrint('id: $id');
+                    _controller.load(id);
+                    
+                  });
+                },
+                child: Text("Play"),
+              ),
             ],
           ),
-        ));
+        ),        
+      );
+    
+   
   }
 }
   
